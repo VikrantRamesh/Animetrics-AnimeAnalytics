95e40c1a18dfd95af68a3c7f2252892dbfad885a
VikrantRamesh <94131187+VikrantRamesh@users.noreply.github.com>
2023-11-12 12:16:30 +0530

    Streamlit Version: 2.1.0
    major changes:
    Adding Comment Section
    GSheet Database
    Minor CSS changes

---
+++ pages/2_📊_Master_Plot.py
@@ -0,0 +1,123 @@
+import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
+import warnings
+from streamlit_plotly_events import plotly_events
+warnings.filterwarnings('ignore')
+import pandas as pd
+
+with open('css/master.css', 'r') as file:
+        css = file.read()
+        st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+with open('css/master_plot.css', 'r') as file:
+        css = file.read()
+        st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+start_year,end_year = st.sidebar.slider("Year Range", 1970, 2025, (1970, 2025))
+
+#Reading the Anime data
+df = pd.read_csv('assets/processed_anime_data.csv')
+
+df = df[(df['Start Year'] >= start_year) & (df['Start Year'] <= end_year)]
+
+
+if 'xaxis' not in st.session_state:
+    st.session_state.xaxis = "Members"
+
+trans_df = df.copy()
+trans_df['popularity'] = max(trans_df['popularity']) - trans_df['popularity'] +1 
+
+main_container = st.container()
+left,right = main_container.columns([3,1])
+with main_container:
+    with left:
+        graph_size = 800
+        slide_left, slide_mid, slide_right = st.columns([1,12,1]) 
+        graph_size = slide_mid.slider("Adjust graph size", 500, 1000, 800, 25)
+        fig = go.Figure()
+        
+        hover ='''
+        <b>Name:</b> %{customdata[0]} <br>
+        <b>Score:</b> %{x} <br>
+        <b>Rank:</b> %{customdata[1]} <br>
+        <b>Members:</b> %{customdata[2]} <br>
+        <b>Episodes:</b> %{customdata[3]} <br>'''
+
+        colors_dict = {'TV': '#FF5733', 'Movie': '#0099FF', 'OVA': ' #99FF33', 'Special': '#B533FF', 'ONA': '#FFA319'}  
+        sym_dict = {'TV': 'cross', 'Movie': 'square', 'OVA': 'diamond', 'Special': 'circle', 'ONA': 'x'}  
+
+
+        for typ in trans_df['Type'].unique():
+            filt = trans_df[trans_df['Type'] == typ]
+            custom_data = np.stack((filt['Title'], filt['Rank'], filt['Members'], filt['Episodes'],filt['Image']), axis=-1)
+            
+            fig.add_trace(go.Scatter(x = filt['Score'],
+                                            y=filt[st.session_state.xaxis],
+                                            customdata = custom_data, 
+                                            mode='markers',
+                                            name=typ,
+                                            marker = dict(color=colors_dict[typ], symbol = sym_dict[typ])))
+            fig.update_traces(hovertemplate=hover)
+
+            
+        
+        fig.update_layout(
+            title = {
+                'text':f'Rating vs {st.session_state.xaxis}',
+                'x':0.5
+            },
+            xaxis=dict(title='Rating',showgrid=False, linecolor='white', showline=True),
+            yaxis=dict(title=st.session_state.xaxis,showgrid=False, linecolor='white', showline=True),
+            showlegend = True,
+            height=600,
+            width=graph_size,
+            paper_bgcolor = 'rgba(0,0,0,0)',
+            plot_bgcolor = 'rgba(0,0,0,0)',
+            font=dict(color='white'),
+            hovermode="closest"
+        )
+
+        if 'prev_point' not in st.session_state:
+            st.session_state.prev_point = []
+            st.session_state.prev_prev_point = []
+            st.session_state.prev_prev_prev_point = []
+        # selected_points = []
+        # prev_point = selected_points
+        # prev_prev_point = prev_point
+        fig.update_layout({"uirevision": "foo"}, overwrite=True)
+        selected_points = plotly_events(fig, hover_event=True, click_event=True)
+        
+        
+        result = []
+        if selected_points:
+            result = df[(trans_df['Score'] == selected_points[0]['x']) & (trans_df[st.session_state.xaxis] == selected_points[0]['y'])]
+
+            # print("prev:",selected_points,st.session_state.prev_point,st.session_state.prev_prev_point)
+            # st.session_state.prev_prev_prev_point = st.session_state.prev_prev_point
+            # st.session_state.prev_prev_point = st.session_state.prev_point
+            # st.session_state.prev_point = selected_points
+            # print(selected_points,st.session_state.prev_point,st.session_state.prev_prev_point,st.session_state.prev_prev_prev_point)
+            # if st.session_state.prev_point == selected_points:
+            #     if st.session_state.prev_prev_point == st.session_state.prev_point and st.session_state.prev_prev_prev_point==st.session_state.prev_prev_point:
+            #         st.success("OKK")
+            #         print("ok_______________________________")
+            #         st.session_state.prev_point = []
+            #         st.session_state.prev_prev_point = []
+            #         st.session_state.prev_prev_prev_point = []
+
+            
+
+    with right:
+
+        with st.sidebar:
+            st.selectbox("Select Y-axis:", ["Members", "popularity"], key="xaxis")
+            
+
+        if len(result)>0:
+
+            st.image(str(result['Image'].values[0]))
+            
+            st.markdown(f'''#### <center> {str(result['Title'].values[0])}\n</center>
+            Rank: {str(result['Rank'].values[0])}<br>Score: {str(result['Score'].values[0])}<br>Popularity: #{str(result['popularity'].values[0])}<br>Episodes: {str(result['Episodes'].values[0])}<br>Score: {str(result['Score'].values[0])}<br>Genre: {str(result['Genre'].values[0])[1:-1]}<br>Start Date: {str(result['Start Date'].values[0])}<br>End Date: {str(result['End Date'].values[0])}           
+            ''', unsafe_allow_html=True)
+
---
+++ pages/3_📫_Contact.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import warnings
+warnings.filterwarnings('ignore')
+from streamlit_gsheets import GSheetsConnection
+import requests
+from datetime import datetime
+import pandas as pd
+
+# url = "https://docs.google.com/spreadsheets/d/1wGHqNlmQnwgM19QlVedBDg6ED-zrWu_qzXrCexp34h4/edit?usp=sharing"
+conn = st.connection("gsheets", type=GSheetsConnection)
+#reading comments data
+sql = '''
+SELECT "Name", "Date", "Comment" FROM Comments WHERE "Name" IS NOT NULL;
+'''
+comment_data = conn.query(worksheet='Comments',sql =sql, ttl=5)
+
+
+st.markdown("## <center>CONTACT ME</center>", unsafe_allow_html=True)
+
+#importing CSS
+with open('css/master.css', 'r') as file:
+        css = file.read()
+        st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+with open('css/contact.css', 'r') as file:
+        css = file.read()
+        st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+
+left, right = st.columns([2,4])
+
+with left:
+    col_left, cent = st.columns([1,8])
+    cent.image("assets/images/Vikrant.jpg",  use_column_width=True)
+
+
+
+with right:
+    st.markdown("###### <b> Name: </b>&emsp;&emsp;&nbsp; Vikrant Ramesh", unsafe_allow_html=True)
+    st.markdown("###### <b> Mail: </b>&emsp;&emsp;&emsp; rvikrant2004@gmail.com", unsafe_allow_html=True)
+    st.markdown("###### <b> Github: </b>&emsp;&emsp; https://github.com/VikrantRamesh", unsafe_allow_html=True)
+    st.markdown("###### <b> Linkedin: </b>&emsp;&nbsp;&nbsp;https://www.linkedin.com/in/vikrant-ramesh-046061190/", unsafe_allow_html=True)
+    st.markdown("###### <b> Github: </b>&emsp;&emsp; https://github.com/VikrantRamesh", unsafe_allow_html=True)
+
+    st.markdown('#####  Comment')
+    # st.markdown('''
+    #     <form action="https://formspree.io/f/mwkdzjpj" method="post">
+    #         <input name="name" placeholder ='Name' class='form_input'/>
+    #         <textarea name="comment" placeholder ='Comment'></textarea>
+    #         <button type="submit" class="btn btn-lg btn-dark btn-block">Submit</button>
+    #     </form>
+    # ''', unsafe_allow_html=True)\
+    with st.form("Comments", clear_on_submit=True):
+        name = st.text_input("Name")
+        comment = st.text_area("Comment")
+
+        form_data = {
+            "name": name,
+            "comment": comment,
+        }
+        submit = st.form_submit_button("Comment")
+
+    # Submit button
+    if submit:
+        
+        response = requests.post("https://formspree.io/f/mwkdzjpj", data=form_data)
+        
+        if response.status_code == 200:
+            st.success("Thanks for Commenting!...Proessing, Please wait")
+        else:
+            st.warning(f"Form submission failed with status code: {response.status_code}")   
+        
+        # Get the current date
+        current_date = datetime.now().strftime("%Y-%m-%d") 
+
+        new_comment = pd.DataFrame([[name,current_date, comment]], columns=comment_data.columns)
+
+        sql = '''
+        SELECT "Name", "Date", "Comment" FROM Comments WHERE "Name" IS NOT NULL;
+        '''
+        
+        comment_data = conn.query(sql=sql)
+        comment_data = pd.concat([comment_data, new_comment], ignore_index=True, axis=0)
+        conn.update(
+            worksheet="Comments",
+            data = comment_data
+        )
+
+
+container_style = (
+    " border-radius: 10px; box-shadow: 2px 2px 5px #888; margin:10px auto;"
+)
+
+st.markdown("### Latest Comments")
+
+for index,row in comment_data[::-1].head().iterrows():
+    st.markdown(
+        f"""
+        <div style = "{container_style}">
+            <div style='background-color:#E5E4E2;padding: 10px; border-radius:10px 10px 0 0;'>
+                <div style="display:flex;color: black;">
+                    <div style="flex: 1; margin-right: 20px;font-size: 20px;">
+                        <strong style="font-weight: bold;">Name:&nbsp;&nbsp;&nbsp;</strong> {row['Name']}
+                    </div>
+                    <div style="flex: 1;text-align: right;font-size: 16px;font-weight: black;">
+                        {row['Date']}
+                    </div>
+                </div>
+            </div>
+            <div style = "padding-right:10px;padding: 10px;">
+                <p><em style="font-style: italic;"> {row['Comment']}</em></p>
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
